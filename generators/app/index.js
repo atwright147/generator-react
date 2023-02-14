61c10573331024f491fe48683874cdd35674a07b
Andy Wright <atwright147@gmail.com>
2023-02-14 00:21:11 +0000

    initial commit

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,29 +1,32 @@
-const Generator = require("yeoman-generator");
+const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-    this.log("Creating component...");
+    this.log('Creating component...');
 
-    this.argument("name", {
+    this.argument('name', {
       type: String,
       required: true,
-      description:
-        "The name of the component. This will also be the name of the directory containing the component.",
+      description: 'The name of the component. This will also be the name of the directory containing the component.',
     });
   }
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath("component.jsx"),
-      this.destinationPath(this.options.name + ".jsx"),
-      {
-        name: this.options.name,
-      }
+      this.templatePath('component.tsx.ejs'),
+      this.destinationPath(`${this.options.name}.tsx`),
+      { name: this.options.name },
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('component.module.scss.ejs'),
+      this.destinationPath(`${this.options.name}.module.scss`),
+      { name: this.options.name },
     );
   }
 
   done() {
-    console.log("Finished!");
+    console.log('Finished!');
   }
 };
--- generators/app/templates/component.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './<%= name %>.css';
-
-export default function <%= name %>() {
-  return <div><%= name %></div>;
-}
